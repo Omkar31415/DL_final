a4a0acb6c1a4d00c48d6d244a8e3bb22cee0008a
jagruthreddy <jagruthreddy9999@gmail.com>
2024-11-26 23:22:46 -0600

    eval eval real

--- evaluate_real.py
+++ evaluate_real.py
@@ -20,6 +20,17 @@ from utils.track_utils import (
     get_gt_corners,
 )
 from utils.visualization import generate_track_colors, render_pred_tracks, render_tracks
+import sys
+
+# Add the project root directory to sys.path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__)))  # Adjust the path if necessary
+sys.path.append(project_root)
+
+#sys.path.append('C:\\Users\\jagru\\Desktop\\Projects\\Project_1\\DL_final_Team1')
+
+print("Current working directory:", os.getcwd())
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 # Configure GPU order
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
@@ -33,19 +44,19 @@ results_table.field_names = ["Inference Time"]
 corner_config = CornerConfig(30, 0.3, 15, 0.15, False, 11)
 
 EvalDatasetConfigDict = {
-    EvalDatasetType.EC: {"dt": 0.010, "root_dir": "<path>"},
+    EvalDatasetType.EC: {"dt": 0.010, "root_dir": "C:\\Users\\jagru\\Desktop\\Projects\\Project_1\\DL_final_Team1\\ec_subseq"},
     EvalDatasetType.EDS: {"dt": 0.005, "root_dir": "<path>"},
 }
 
 EVAL_DATASETS = [
-    ("peanuts_light_160_386", EvalDatasetType.EDS),
-    ("rocket_earth_light_338_438", EvalDatasetType.EDS),
-    ("ziggy_in_the_arena_1350_1650", EvalDatasetType.EDS),
-    ("peanuts_running_2360_2460", EvalDatasetType.EDS),
-    ("shapes_translation_8_88", EvalDatasetType.EC),
-    ("shapes_rotation_165_245", EvalDatasetType.EC),
-    ("shapes_6dof_485_565", EvalDatasetType.EC),
-    ("boxes_translation_330_410", EvalDatasetType.EC),
+    #("peanuts_light_160_386", EvalDatasetType.EDS),
+    #("rocket_earth_light_338_438", EvalDatasetType.EDS),
+    #("ziggy_in_the_arena_1350_1650", EvalDatasetType.EDS),
+    #("peanuts_running_2360_2460", EvalDatasetType.EDS),
+    #("shapes_translation_8_88", EvalDatasetType.EC),
+    #("shapes_rotation_165_245", EvalDatasetType.EC),
+    #("shapes_6dof_485_565", EvalDatasetType.EC),
+    #("boxes_translation_330_410", EvalDatasetType.EC),
     ("boxes_rotation_198_278", EvalDatasetType.EC),
 ]
 
@@ -74,6 +85,7 @@ def evaluate(model, sequence_dataset, dt_track_vis, sequence_name, visualize):
                 sequence_dataset.accumulate_y_hat(y_hat)
             tracks_pred.add_observation(t, sequence_dataset.u_centers.cpu().numpy())
 
+
         if visualize:
             # Visualize network tracks
             gif_img_arr = []
@@ -93,12 +105,15 @@ def evaluate(model, sequence_dataset, dt_track_vis, sequence_name, visualize):
             imageio.mimsave(f"{sequence_name}_tracks_pred.gif", gif_img_arr)
 
     # Save predicted tracks
+    output_path = os.path.join(os.getcwd(), f"{sequence_name}.txt")
+    print(f"Saving predicted tracks to: {output_path}")
     np.savetxt(
-        f"{sequence_name}.txt",
+        output_path,
         tracks_pred.track_data,
         fmt=["%i", "%.9f", "%i", "%i"],
         delimiter=" ",
     )
+
 
     metrics = {}
     metrics["latency"] = sum(cuda_timers[sequence_dataset.sequence_name])
@@ -118,6 +133,7 @@ def track(cfg):
     model = hydra.utils.instantiate(cfg.model, _recursive_=False)
 
     state_dict = torch.load(cfg.weights_path, map_location="cuda:0")["state_dict"]
+
     model.load_state_dict(state_dict)
     if torch.cuda.is_available():
         model = model.cuda()
@@ -148,7 +164,7 @@ def track(cfg):
 
         dataset.override_keypoints(gt_start_corners)
 
-        metrics = evaluate(model, dataset, cfg.dt_track_vis, seq_name, cfg.visualize)
+        metrics = evaluate(model, dataset, cfg.dt_track_vis, seq_name,True)
 
         logger.info(f"=== DATASET: {seq_name} ===")
         logger.info(f"Latency: {metrics['latency']} s")
